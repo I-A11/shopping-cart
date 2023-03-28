4051d244a51abe0003c5380471c2dfbc4af73d6f
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-03-28 12:08:23 +1100

    Create generateCartItems function.

--- src/cart.js
+++ src/cart.js
@@ -1,3 +1,6 @@
+const label = document.querySelector("#label");
+const shoppingCart = document.querySelector("#shopping-cart");
+
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 const calculation = () => {
@@ -7,3 +10,35 @@ const calculation = () => {
 };
 
 calculation();
+
+const generateCartItems = () => {
+  if (basket.length !== 0) {
+    return (shoppingCart.innerHTML = basket
+      .map((x) => {
+        const { id, item } = x;
+        const search = shopItemsData.find((y) => y.id === id) || [];
+        return `
+        <div class="cart-item">
+            <img width="100" src=${search.img} alt=${search.name} />
+            <div class="details">
+                <div class="title-price-x"></div>
+
+                <div class="cart-buttons"></div>
+                <h3></h3>
+            </div>
+        </div>
+        `;
+      })
+      .join(""));
+  } else {
+    shoppingCart.innerHTML = ``;
+    label.innerHTML = `
+    <h2>Cart is Empty</h2>
+    <a href="index.html">
+    <button class="homeBtn">Back to Home</button>
+    </a>
+    `;
+  }
+};
+
+generateCartItems();
